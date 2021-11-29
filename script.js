7f9525c2f8b47e19bcf6b94d0e4a7a0b26859316
unknown <stanislav.jarysta@codeacademylt.onmicrosoft.com>
2021-11-29 12:06:46 +0200

    random num1 and num2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("pirmas komitas")
+// const random_number = Math.random();
+//
+// alert(number);
+//
+// const random_number = Math.round(Math.random() * 10);
+
+function myRandom (from, to)  {
+    return Math.round((Math.random() * (to - from + 1)) + from);
+}
+
+let rand_num1 = myRandom(1,9);   //задаем диапазон для первого числа от 1 до 9
+rand_num2 = myRandom(1,7);   //задаем диапазон для второго числа от 1 до 7
+sum = rand_num1 + rand_num2; //вычисляем сумму двух случайных чисел
+console.log(rand_num1)
